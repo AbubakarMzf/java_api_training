f2bdd50816e064c93f2c3815c97bbd4f940689cb
AbubakarMzf <abubakar.mzf@gmail.com>
2022-03-18 16:20:30 +0100

    Bug fixture of start route

--- src/main/java/fr/lernejo/navy_battle/startHandler.java
+++ src/main/java/fr/lernejo/navy_battle/startHandler.java
@@ -1,29 +1,31 @@
 package fr.lernejo.navy_battle;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.sun.net.httpserver.Headers;
+
 import com.sun.net.httpserver.HttpExchange;
 import com.sun.net.httpserver.HttpHandler;
-import com.sun.net.httpserver.HttpPrincipal;
+
 
 import java.io.*;
-import java.net.HttpURLConnection;
+
 import java.nio.charset.StandardCharsets;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-import java.util.Set;
-import java.util.concurrent.Exchanger;
+
 
 public class startHandler implements HttpHandler {
     @Override
     public void handle(HttpExchange exchange) throws IOException {
         if (checkVerb(exchange)) {
-            ObjectMapper objectMapper = new ObjectMapper();
-            InputStream fileInputStream = exchange.getRequestBody();
-            String body = new String(fileInputStream.readAllBytes(), StandardCharsets.UTF_8);
-            StartSchema startSchema = objectMapper.readValue(body, StartSchema.class);
-            response(startSchema, exchange);
+            try {
+                ObjectMapper objectMapper = new ObjectMapper();
+                InputStream fileInputStream = exchange.getRequestBody();
+                String body = new String(fileInputStream.readAllBytes(), StandardCharsets.UTF_8);
+                StartSchema startSchema = objectMapper.readValue(body, StartSchema.class);
+                response(startSchema, exchange);
+            }
+            catch (Exception e){
+                exchange.sendResponseHeaders(404 - 4, "Erreur".length());
+                exchange.getResponseBody().write("Erreur".getBytes());;
+            }
         }
         else {
             exchange.sendResponseHeaders(404, "Erreur".length());
