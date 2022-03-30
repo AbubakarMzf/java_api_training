36f3f9032ff72a60840e0ef79e345cdbd29b76ad
Abubakar Mzf <72223629+AbubakarMzf@users.noreply.github.com>
2022-03-30 17:46:10 +0200

    Update Readme for coverage badge.

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # java_api_training
 ![example branch parameter](https://github.com/github/docs/actions/workflows/main.yml/badge.svg?branch=feature-1)
+[![codecov](https://codecov.io/gh/AbubakarMzf/java_api_training/branch/main/graph/badge.svg)](https://codecov.io/gh/AbubakarMzf/java_api_training)
--- src/test/java/fr/lernejo/navy_battle/pingHandlerTest.java
+++ src/test/java/fr/lernejo/navy_battle/pingHandlerTest.java
@@ -1,11 +1,27 @@
 package fr.lernejo.navy_battle;
+import com.sun.net.httpserver.HttpServer;
 import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
 
 class pingHandlerTest {
-
-    void test_good_ping() {
-        int result = 1;
-        int expected = 1;
-        Assertions.assertThat(result).as("OK").isEqualTo(1);
+    @Test
+    void test_route_ping() {
+        try {
+            Server serverClass = new Server(5000);
+            HttpServer server = serverClass.createServer(serverClass);
+            server.start();
+            HttpClient client = HttpClient.newHttpClient();
+            HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create("http://localhost:5000/ping"))
+                .GET()
+                .build();
+            HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+            Assertions.assertThat(response.statusCode()).isEqualTo(404);
+        }catch(Exception e) {}
     }
 }
